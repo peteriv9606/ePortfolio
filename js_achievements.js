bf54ba6a1655534e02300f37d0407bfe5764bbb7
Petar Ivanov <pr0princ3@gmail.com>
2020-11-28 14:40:08 +0000

    Formatting

--- js_achievements.js
+++ js_achievements.js
@@ -8,24 +8,23 @@ var currCourse = "";
 var clickTrigger = window.location.search.substring(1);
 
 window.onload = function() {
-    if(clickTrigger)
-    loadAch(clickTrigger);
+    if (clickTrigger)
+        loadAch(clickTrigger);
 }
 
-function expand(menu){
-    
+function expand(menu) {
+
     // alert("Pressed Expand with menu: " + menu + " First menu = "+ firstMenu);
-    if(firstMenu==0){
+    if (firstMenu == 0) {
         //first time
         //alert("Pressed "+menu+" for the first time!");
-        firstMenu=1;
+        firstMenu = 1;
         oldMenu = menu;
-        document.getElementById(menu).style.display = "block";  
+        document.getElementById(menu).style.display = "block";
         document.getElementById('selected').innerHTML = "> " + menu;
         currCourse = document.getElementById('selected').innerHTML;
-    }
-    else{
-        if(oldMenu != menu){
+    } else {
+        if (oldMenu != menu) {
             //change (different)
             //alert("Changing menu's.. (Was" + oldMenu + ", now is pressed "+ menu + ")");
             document.getElementById(oldMenu).style.display = "none";
@@ -34,15 +33,14 @@ function expand(menu){
             currCourse = document.getElementById('selected').innerHTML;
             oldMenu = menu;
             loadAch("instructions");
-        }
-        else{
+        } else {
             //pressed again
             //alert("Pressed again! (" + menu + " = " + oldMenu + "). Now Hiding.");
-            
+
             document.getElementById(menu).style.display = "none";
             loadAch("instructions");
             isinstruct = 1;
-            document.getElementById('selected').innerHTML = "" ;
+            document.getElementById('selected').innerHTML = "";
             firstMenu = 0;
             oldMenu = "";
             menu = "";
@@ -51,43 +49,39 @@ function expand(menu){
     }
 }
 
-function loadAch(ach){
-    
-    if(first==0){
+function loadAch(ach) {
+
+    if (first == 0) {
         //first time (initialization)
-        if(ach != "instructions"){
+        if (ach != "instructions") {
             //achievement is NOT INSTRUCTIONS
             document.getElementById("instructions").style.display = "none";
-            isinstruct=0;
+            isinstruct = 0;
             //alert("Pressed "+ach+" for the first time!");
-            first=1;
+            first = 1;
             oldach = ach;
             document.getElementById(ach).style.display = "block";
             currCourse = document.getElementById('selected').innerHTML;
-            document.getElementById('navi').innerHTML += "<a id='sub' style='cursor:pointer;'> > " + ach +"</a> ";
+            document.getElementById('navi').innerHTML += "<a id='sub' style='cursor:pointer;'> > " + ach + "</a> ";
         }
-        
-    }
-    else{
+
+    } else {
         //not the first time (already initialized)
-        if(oldach!=ach){
+        if (oldach != ach) {
             //changing between achievements (new achievement selected)
             //alert("Changing ach's.. (Was " + oldach + ", now is pressed "+ ach + ")");
             document.getElementById('sub').innerHTML = "";
             document.getElementById(oldach).style.display = "none";
-            if(ach != "instructions")
-            {
+            if (ach != "instructions") {
                 //changing achievement (not instructions)
                 document.getElementById(ach).style.display = "block";
                 document.getElementById('sub').innerHTML = " > " + ach;
                 //document.getElementById('selected').innerHTML = currCourse + " > " + ach;
                 oldach = ach;
-            }
-            else
-            {
+            } else {
                 //changing achievement (INSTRUCTION PAGE)
                 isinstruct = 1;
-                document.getElementById('navi').innerHTML = "<a id='selected' onclick='hideSub()' style='cursor: pointer;'>"+currCourse+"</a>"
+                document.getElementById('navi').innerHTML = "<a id='selected' onclick='hideSub()' style='cursor: pointer;'>" + currCourse + "</a>"
                 document.getElementById(ach).style.display = "none";
                 document.getElementById("instructions").style.display = "flex";
                 document.getElementById('selected').innerHTML = "";
@@ -95,8 +89,7 @@ function loadAch(ach){
                 oldach = "";
                 document.getElementById('selected').innerHTML = currCourse;
             }
-        }
-        else{
+        } else {
             //changing between achievements (same achievement selected)
             //alert("Pressed again! (" + ach + " = " + oldach + "). Now Hiding."); 
             document.getElementById(ach).style.display = "none";
@@ -104,14 +97,14 @@ function loadAch(ach){
             document.getElementById('selected').innerHTML = "";
             first = 0;
             oldach = "";
-            document.getElementById('navi').innerHTML = "<a id='selected' onclick='hideSub()' style='cursor: pointer;'>"+currCourse+"</a>"
+            document.getElementById('navi').innerHTML = "<a id='selected' onclick='hideSub()' style='cursor: pointer;'>" + currCourse + "</a>"
         }
     }
 }
 
-function hideSub(){
+function hideSub() {
     isinstruct = 1;
-    document.getElementById('navi').innerHTML = "<a id='selected' onclick='hideSub()' style='cursor: pointer;'>"+currCourse+"</a>"
+    document.getElementById('navi').innerHTML = "<a id='selected' onclick='hideSub()' style='cursor: pointer;'>" + currCourse + "</a>"
     document.getElementById(oldach).style.display = "none";
     document.getElementById("instructions").style.display = "flex";
     first = 0;
--- js_assessment.js
+++ js_assessment.js
@@ -3,36 +3,32 @@ var oldWeek = "";
 var isinstruct = 1;
 var clickTrigger = window.location.search.substring(1);
 window.onload = function() {
-    if(clickTrigger)
-    loadWeek(clickTrigger);
+    if (clickTrigger)
+        loadWeek(clickTrigger);
 }
 
-function loadWeek(week){
-    if(first==0){
+function loadWeek(week) {
+    if (first == 0) {
         //first time
-        if(week != "instructions"){
-        document.getElementById("instructions").style.display = "none";
-        isinstruct=0;
-        //alert("Pressed "+week+" for the first time!");
-        first=1;
-        oldWeek = week;
-        document.getElementById(week).style.display = "block";
-        document.getElementById('selected').innerHTML = "> " + week;
+        if (week != "instructions") {
+            document.getElementById("instructions").style.display = "none";
+            isinstruct = 0;
+            //alert("Pressed "+week+" for the first time!");
+            first = 1;
+            oldWeek = week;
+            document.getElementById(week).style.display = "block";
+            document.getElementById('selected').innerHTML = "> " + week;
         }
-        
-    }
-    else{
-        if(oldWeek!=week){
+
+    } else {
+        if (oldWeek != week) {
             //alert("Changing weeks.. (Was" + oldWeek + ", now is ressed "+ week + ")");
             document.getElementById(oldWeek).style.display = "none";
-            if(week != "instructions")
-            {
+            if (week != "instructions") {
                 document.getElementById(week).style.display = "block";
                 document.getElementById('selected').innerHTML = "> " + week;
-                oldWeek=week;
-            }
-            else
-            {
+                oldWeek = week;
+            } else {
                 isinstruct = 1;
                 document.getElementById(week).style.display = "none";
                 document.getElementById("instructions").style.display = "flex";
@@ -40,8 +36,7 @@ function loadWeek(week){
                 first = 0;
                 oldWeek = "";
             }
-        }
-        else{
+        } else {
             //alert("Pressed again! (" + week + " = " + oldWeek + "). Now Hiding.");
             document.getElementById(week).style.display = "none";
             document.getElementById("instructions").style.display = "flex";
