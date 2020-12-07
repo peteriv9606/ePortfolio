8c103c1a19f5a31355b0bf68c75e09e8a7fa7d81
Petar Ivanov <pr0princ3@gmail.com>
2020-12-07 18:50:09 +0000

    footerJQuery

--- js_index_footer_loadPage.js
+++ js_index_footer_loadPage.js
@@ -1,51 +1,20 @@
-var arr = [];
-var first = true;
 $(document).ready(function(){
     var link = window.location.href;
+    
     if(link.includes("about.html") || link.includes("index.html")){
         $(".left-side").css({"width":"0%"});
         $(".right-side").css({"width":"100%","margin":"0% 10%"});
-     }
-     $(".s1").animate({"opacity":"1"},"slow");
+    }
+    $(".s1").animate({"opacity":"1"},"slow");
+     
+    //sitemap toggle buttons
+    $(".expandBtn").click(function(){
+        $('#'+this.value).toggle("fast");
+    });
+    
 });
-
-
-
 
 function loadPage(page) {
     window.location.href = page;
-
-    
-}
-
- function expandFooter(thisID) {
-    //check if existing
-    if (arr.length == 0) {
-        first = true;
-    }
-    if (first) {
-        //if arr is empty - add this as a first element
-        //alert("First element arr.push("+thisID+ "). Display = block")
-        document.getElementById(thisID).style.display = "block";
-        arr.push(thisID);
-        first = false;
-    } else {
-        //if arr is NOT empty - add this as a next element
-        //alert("NOT First element.");
-        for (var i = 0; i < arr.length; i++) {
-            if (thisID == arr[i]) {
-                //alert("ID found: arr["+ i +"]="+arr[i]+" = thisID["+thisID+"]! Now Hiding..");
-                document.getElementById(arr[i]).style.display = "none";
-                var index = arr.indexOf(arr[i]);
-                if (index > -1) {
-                    arr.splice(index, 1);
-                    return 0;
-                }
-            }
-        }
-        //alert("Element not found. Now Adding arr["+i+"] = "+thisID+".");
-        document.getElementById(thisID).style.display = "block";
-        arr[i] = thisID;
-    }
 }
 
