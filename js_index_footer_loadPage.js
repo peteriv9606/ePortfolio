af88657430d5a0d91f29e43e025af8481252b098
Petar Ivanov <pr0princ3@gmail.com>
2020-12-15 12:09:14 +0000

    upd

--- js_index_footer_loadPage.js
+++ js_index_footer_loadPage.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
     var link = window.location.href;
+    $("main").animate({"opacity":"1"},"slow");
     
     if(link.includes("about.html") || link.includes("index.html")){
         $(".left-side").css({"width":"0%"});
         $(".right-side").css({"width":"100%","margin":"0%"});
     }
-    $(".s1").animate({"opacity":"1"},"slow");
+    
      
     //sitemap toggle buttons
     $(".expandBtn").click(function(){
